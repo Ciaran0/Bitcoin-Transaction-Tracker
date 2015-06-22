36d06c5458828ecaf22083167d04fe6ede8af4e0
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2015-06-22 22:42:02 +0100

    refactored routes into seperate file

--- routes/users.js
+++ routes/users.js
@@ -7,31 +7,15 @@ var jwt = require('express-jwt');
 
 var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
-router.post('/api/signup', function(req, res, next){
-  if(!req.body.username || !req.body.password){
-    return res.status(400).json({message: 'Please fill out all fields'});
-  }
-
-  var user = new User();
-
-  user.username = req.body.username;
-
-  user.setPassword(req.body.password)
-
-  user.save(function (err){
-    if(err){ return next(err); }
-
-    return res.json({token: user.generateJWT()})
-  });
-});
-
-
+//login route
 router.post('/api/login', function(req, res, next){
   if(!req.body.username || !req.body.password){
     return res.status(400).json({message: 'Please fill out all fields'});
   }
+  console.log(req.payload);
 
   passport.authenticate('local', function(err, user, info){
+    console.log(user);
     if(err){ return next(err); }
 
     if(user){
@@ -40,6 +24,21 @@ router.post('/api/login', function(req, res, next){
       return res.status(401).json(info);
     }
   })(req, res, next);
+});
+
+//signup/register route
+router.post('/api/signup', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+  var user = new User();
+  user.username = req.body.username;
+  user.setPassword(req.body.password)
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
 module.exports = router;
