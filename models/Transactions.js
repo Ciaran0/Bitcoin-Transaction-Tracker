f031e76d50d8b20c79dee79dfc757b7222b84f8e
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2015-06-24 09:02:19 +0100

    Transaction model started

--- models/Transactions.js
+++ models/Transactions.js
@@ -1,1 +1,12 @@
+var mongoose = require('mongoose');
+var TransactionSchema = new mongoose.Schema({
+  buyAmount: Number,
+  alertHigh: Number,
+  alertLow: Number,
+  alert: Boolean,
+  date: Date,
+  weekValue, [Number],
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
 
+mongoose.model('Transaction', TransactionSchema);
