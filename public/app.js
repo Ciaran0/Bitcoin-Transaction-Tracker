46cc3c7a729851b0627e15b54e1e967784190e94
coconnor <ciaran.o'connor@paddypower.com>
2015-07-17 12:38:13 +0100

    Restricting access to loggedin users

--- public/app.js
+++ public/app.js
@@ -1,26 +1,36 @@
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+  .config(['$locationProvider','$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'MainCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    })
+    .when('/signup', {
+      templateUrl: 'views/signup.html',
+      controller: 'SignupCtrl'
+    })
+    .when('/profile' , {
+      templateUrl: 'views/profile.html',
+      controller: 'ProfileCtrl',
+      resolve: {
+        token: 'token',
+        load: function($q, $location, token) {
+          var deferred = $q.defer();
+          deferred.resolve();
+          if (!token.isLoggedIn()) {
+             $location.path('/login');
+          }
+          return deferred.promise;
+        }
+      }
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
-  angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-    .config(['$locationProvider','$routeProvider', function($locationProvider, $routeProvider) {
-      $locationProvider.html5Mode(true);
-      $routeProvider
-      .when('/', {
-        templateUrl: 'views/home.html',
-        controller: 'MainCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .when('/signup', {
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl'
-      })
-      .when('/profile' , {
-        templateUrl: 'views/profile.html',
-        controller: 'ProfileCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-    }]);
+  }]);
